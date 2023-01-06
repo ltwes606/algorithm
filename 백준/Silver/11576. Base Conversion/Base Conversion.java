4a079bc7e8d4acfd4876a32bc947e2ac66d43ef4
ltwes606 <121424789+ltwes606@users.noreply.github.com>
2023-01-06 16:11:51 +0900

    [Silver V] Title: Base Conversion, Time: 132 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Silver/11576. Base Conversion/Base Conversion.java
+++ 백준/Silver/11576. Base Conversion/Base Conversion.java
@@ -3,12 +3,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
 
 public class Main {
 
