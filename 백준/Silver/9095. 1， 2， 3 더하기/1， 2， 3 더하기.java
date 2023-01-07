ec4f558b0068c448236445d407bbe3cfff01c370
ltwes606 <121424789+ltwes606@users.noreply.github.com>
2023-01-07 22:53:15 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int repeat = Integer.parseInt(reader.readLine());
+        int[] cases = new int[11];
+        cases[0] = 0;
+        cases[1] = 1;
+        cases[2] = 2;
+        cases[3] = 4;
+        for (int i = 4; i < cases.length; i++) {
+            cases[i] = cases[i - 3] + cases[i - 2] + cases[i - 1];
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < repeat; i++) {
+            int input = Integer.parseInt(reader.readLine());
+            result.append(cases[input]);
+            result.append('\n');
+        }
+        reader.close();
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        writer.write(result.toString());
+        writer.close();
+    }
+}
