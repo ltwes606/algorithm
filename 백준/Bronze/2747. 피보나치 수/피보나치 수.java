aa6439f69bb1ca9b9da9e4156f052ee1a4a62ca7
ltwes606 <121424789+ltwes606@users.noreply.github.com>
2023-01-16 22:00:05 +0900

    [Bronze II] Title: 피보나치 수, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static final int MAX_SIZE = 45;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int ordinal = Integer.parseInt(reader.readLine());
+        reader.close();
+
+        int[] dp = new int[MAX_SIZE + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= ordinal; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        System.out.println(dp[ordinal]);
+    }
+}
