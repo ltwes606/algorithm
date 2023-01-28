992d3e5ea8eb371b5c3a09b63750754987010223
ltwes606 <121424789+ltwes606@users.noreply.github.com>
2023-01-28 16:24:16 +0900

    [Silver III] Title: N과 M (7), Time: 1360 ms, Memory: 375620 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+class Main {
+
+    private static final StringBuilder outputStatement = new StringBuilder();
+    private static int[] sequence;
+
+    private static void dfs(int start, int range, int size,
+            ArrayList<Integer> included) {
+        // size만큼 추가되었다면 출력문에 추가
+        if (included.size() == size) {
+            outputStatement.append(included.stream().map(Object::toString)
+                    .collect(Collectors.joining(" ")) + "\n");
+            return;
+        }
+
+        // 숫자 추가
+        for (int i = start; i < range; i++) {
+            Integer numberToAdd = sequence[i];
+
+            included.add(numberToAdd);
+            dfs(0, range, size, included);
+            // 탐색이 끝났다면 추가한 숫자 제거
+            included.remove(included.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int range = Integer.parseInt(tokenizer.nextToken());
+        int size = Integer.parseInt(tokenizer.nextToken());
+        sequence = Arrays.stream(reader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        reader.close();
+
+        // 받은 수열 정렬
+        Arrays.sort(sequence);
+
+        dfs(0, range, size, new ArrayList<Integer>());
+        System.out.print(outputStatement);
+    }
+}
