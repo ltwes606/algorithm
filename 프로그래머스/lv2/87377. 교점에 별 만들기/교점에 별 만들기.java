eb289cdd3c3f170efc8fa893f175695ef6ff37c0
ltwes606 <121424789+ltwes606@users.noreply.github.com>
2023-03-24 15:48:28 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.34 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,60 +1,23 @@
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
 
-    static class Coordinates {
-
-        private final int x;
-        private final int y;
-
-        public Coordinates(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Coordinates that = (Coordinates) o;
-            return x == that.x && y == that.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
-
-    public static final int MIN_VALUE = Integer.MIN_VALUE;
-    public static final int MAX_VALUE = Integer.MAX_VALUE;
+    public static final int MIN_VALUE = -100_000;
+    public static final int MAX_VALUE = 100_000;
     public static final char STAR = '*';
-    public static final char DOT = '.';
 
     public String[] solution(int[][] line) {
         // 교점(x, y) 구하기
-        Set<Coordinates> nodes = new HashSet<>();
+        List<int[]> nodes = new ArrayList<>();
         for (int i1 = 0; i1 + 1 < line.length; i1++) {
             int[] le1 = line[i1];
 
             for (int i2 = i1 + 1; i2 < line.length; i2++) {
                 int[] le2 = line[i2];
 
-                Coordinates newNode = findNode(le1, le2);
+                int[] newNode = findNode(le1, le2);
                 if (newNode == null) {
                     continue;
                 }
@@ -70,31 +33,34 @@ public class Solution {
         int maxY = getMaxY(nodes);
 
         // 최소,최대값을 이용하여 빈 맵 만들기
-        boolean[][] stars = new boolean[(int) (maxY - minY + 1)][(int) (maxX - minX + 1)];
+        char[][] chars = new char[maxY - minY + 1][maxX - minX + 1];
 
         // 생성한 빈 맵에 정수로 이루어진 교점과 최소,최대값을 이용하여 점 찍기
-        for (Coordinates node : nodes) {
-            int y = (maxY - node.getY());
-            int x = (node.getX() - minX);
-            stars[y][x] = true;
+        for (int i = 0; i < nodes.size(); i++) {
+            int[] node = nodes.get(i);
+            int row = maxY - node[1];
+            int col = node[0] - minX;
+            chars[row][col] = STAR;
         }
 
-        String[] result = new String[stars.length];
-        for (int y = 0; y < stars.length; y++) {
-            StringBuilder builder = new StringBuilder();
-            for (int x = 0; x < stars[y].length; x++) {
-                if (stars[y][x]) {
-                    builder.append(STAR);
+        // 아무 것도 안 찍혀있다면 "." 입력
+        for (int row = 0; row < chars.length; row++) {
+            for (int col = 0; col < chars[row].length; col++) {
+                if (chars[row][col] == STAR) {
                     continue;
                 }
-                builder.append(DOT);
+                chars[row][col] = '.';
             }
-            result[y] = builder.toString();
+        }
+
+        String[] result = new String[chars.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = String.valueOf(chars[i]);
         }
         return result;
     }
 
-    private static Coordinates findNode(int[] le1, int[] le2) {
+    private static int[] findNode(int[] le1, int[] le2) {
         long A = le1[0];
         long B = le1[1];
         long E = le1[2];
@@ -104,23 +70,23 @@ public class Solution {
         long F = le2[2];
 
         long denominator = A * D - B * C;
-        if (denominator == 0L) {
+        if (denominator == 0) {
             return null;
         }
 
         long xNumerator = B * F - E * D;
         long yNumerator = E * C - A * F;
-        if (xNumerator % denominator != 0L
-                || yNumerator % denominator != 0L) {
+        if (xNumerator % denominator != 0
+                || yNumerator % denominator != 0) {
             return null;
         }
-        return new Coordinates((int) (xNumerator / denominator), (int) (yNumerator / denominator));
+        return new int[]{(int) (xNumerator / denominator), (int) (yNumerator / denominator)};
     }
 
-    private int getMinX(Set<Coordinates> nodes) {
+    private int getMinX(List<int[]> nodes) {
         int result = MAX_VALUE;
-        for (Coordinates node : nodes) {
-            int x = node.getX();
+        for (int i = 0; i < nodes.size(); i++) {
+            int x = nodes.get(i)[0];
             if (result > x) {
                 result = x;
             }
@@ -128,10 +94,10 @@ public class Solution {
         return result;
     }
 
-    private int getMaxX(Set<Coordinates> nodes) {
+    private int getMaxX(List<int[]> nodes) {
         int result = MIN_VALUE;
-        for (Coordinates node : nodes) {
-            int x = node.getX();
+        for (int i = 0; i < nodes.size(); i++) {
+            int x = nodes.get(i)[0];
             if (result < x) {
                 result = x;
             }
@@ -139,10 +105,10 @@ public class Solution {
         return result;
     }
 
-    private int getMinY(Set<Coordinates> nodes) {
+    private int getMinY(List<int[]> nodes) {
         int result = MAX_VALUE;
-        for (Coordinates node : nodes) {
-            int y = node.getY();
+        for (int i = 0; i < nodes.size(); i++) {
+            int y = nodes.get(i)[1];
             if (result > y) {
                 result = y;
             }
@@ -150,10 +116,10 @@ public class Solution {
         return result;
     }
 
-    private int getMaxY(Set<Coordinates> nodes) {
+    private int getMaxY(List<int[]> nodes) {
         int result = MIN_VALUE;
-        for (Coordinates node : nodes) {
-            int y = node.getY();
+        for (int i = 0; i < nodes.size(); i++) {
+            int y = nodes.get(i)[1];
             if (result < y) {
                 result = y;
             }
