b1e5854cc760f4939d7998b7836f98afc6d9002e
ltwes606 <121424789+ltwes606@users.noreply.github.com>
2023-04-27 13:42:39 +0900

    [unrated] Title: 요격 시스템, Time: 0.45 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -4,36 +4,24 @@ class Solution {
     public int solution(int[][] targets) {
         // 깊은 복사
         int[][] sortedTargets = new int[targets.length][];
-        for (int i = 0; i < sortedTargets.length; i++){
+        for (int i = 0; i < sortedTargets.length; i++) {
             sortedTargets[i] = targets[i].clone();
         }
         
         // 시간순 정렬
-        Arrays.sort(sortedTargets, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] target1, int[] target2){
-                return target1[0] - target2[0];
-            }
-        });
+        Arrays.sort(sortedTargets, (target1, target2) -> target1[0] - target2[0]);
         
         // 계산
         int result = 0;
-        Queue<Integer> queue = new PriorityQueue<>();
-        for (int time = 0, i = 0; time <= 100_000_000; time++){
-            if (!queue.isEmpty() && queue.peek() == time) {
-                queue.clear();
+        int defenseTime = -1;
+        for (int i = 0; i < sortedTargets.length; i++) {
+            int[] target = sortedTargets[i];
+            if (defenseTime < target[0]) {
                 result++;
+                defenseTime = target[1] - 1;
+            } else if (defenseTime > target[1] - 1) {
+                defenseTime = target[1] - 1;
             }
-            
-            while (i < sortedTargets.length && sortedTargets[i][0] == time) {
-                queue.offer(sortedTargets[i][1]);
-                i++;
-            }
-        }
-        
-        if (!queue.isEmpty()) {
-            queue.clear();
-            result++;
         }
         return result;
     }
